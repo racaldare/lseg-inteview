1bd21df7c3996b6dc6243f68c573bcde3890a2ab
Roman Caldare <racaldare@gmail.com>
2025-09-02 22:36:48 +0100

    Implement read file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+const inputFile = path.join(__dirname, "files/input.txt");
+const logFile = path.join(__dirname, "files/output.txt");
+
 /**
  * Generates a report of job executions based on their durations and completion status.
  */
 function GenerateReport(){
+    fs.readFile(inputFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading log file:", err);
+            return;
+        }
 
+        console.log(data);
+    });
 }
 
 
